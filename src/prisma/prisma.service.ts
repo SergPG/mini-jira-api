b4e04df5f50766cc192c2b25faac767066022790
SergPG <serg-popov@ukr.net>
2026-06-10 10:56:39 +0300

    add method findAll

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,24 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private configService: ConfigService) {
+    const dbUrl = configService.get<string>('DATABASE_URL');
+
+    if (!dbUrl) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    const adapter = new PrismaPg({
+      connectionString: dbUrl,
+    });
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
